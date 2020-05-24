ff7ce425b9042f158516e443948f0d78a5c70c22
XuPeng-SH <xupeng3112@163.com>
2020-05-24 22:43:04 +0800

    refactor examples

--- examples/node.py
+++ examples/node.py
@@ -4,10 +4,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append('/home/xupeng/github/pyraftlib/pyraftlib')
 
 import logging
+import fire
 from pyraftlib.service import Service
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(message)s (%(filename)s:%(lineno)d)')
 
-service = Service('example2.yml')
-service.run()
+def Run(yaml_path):
+    service = Service(yaml_path)
+    service.run()
+
+if __name__ == '__main__':
+    fire.Fire(Run)
